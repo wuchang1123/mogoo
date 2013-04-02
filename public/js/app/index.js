d92342c36cf0fb8b434e7df523e9bd3b3808a31d
wuchang1123 <wuchang1123@163.com>
2013-04-03 00:11:05 +0800

    publish js

---
+++ public/js/app/index.js
@@ -0,0 +1,1 @@
+$(function(){var $wrap=$("#index-mos"),jsonPath=["/api/mos/news.json?ajax=1","start=","limit=10"],moList;moList=new MG.WaterFall($wrap,{});moList.toload=function(cb){MG.renderTemplate("modules/index/molist-news",jsonPath.join("&"),function(html,data){var list=data&&data.json.molist;if(list){var mo=_.last(list);mo&&(jsonPath[1]="start="+mo.mid)}cb&&cb(html)})}});
